b6215b0b91c376b3abead8c85f6189e889058905
Wei <weihuangv@gmail.com>
2017-03-06 04:47:20 +0800

    fix star rating

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  resources :courses
-
-  
+  resources :courses 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
