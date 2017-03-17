c52da14f4a8d33bf286c3e505d10f8a96aae0054
Wei <weihuangv@gmail.com>
2017-03-17 23:03:58 +0800

    Fix FB login email cant be blank problem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308115019) do
+ActiveRecord::Schema.define(version: 20170317145934) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20170308115019) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
+    t.string   "email",                  default: ""
     t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20170308115019) do
     t.boolean  "admin",                  default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email", "provider", "uid"], name: "index_users_on_identity", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
